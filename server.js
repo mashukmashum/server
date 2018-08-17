3e4ff8b3e08bcb94403098d804af77975a64542f
mashukmashum <mashukgce@gmail.com>
2018-08-17 14:11:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,9 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
-var cors = require('cors')
 
 const server = express()
-server.use(cors({
-  'allowedHeaders': ['sessionId', 'Content-Type'],
-  'exposedHeaders': ['sessionId'],
-  'origin': '*',
-  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  'preflightContinue': false
-}));
-  server.use((req, res) => res.sendFile(INDEX) )
+  .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const io = socketIO(server);
